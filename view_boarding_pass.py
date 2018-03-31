dc7b3d6a0c703bd2bef703a2b496d0f736d5d633
Sourav Johar <johar.sourav97@gmail.com>
2018-03-31 20:00:45 +0530

    cleanup

--- view_boarding_pass.py
+++ view_boarding_pass.py
@@ -11,65 +11,33 @@ from database.a320 import a320
 import pyqrcode
 import webbrowser
 
-# TODO
-# boarding pass generator backend and frontend
 
 conn = db.connect('database/kingsbase.db')
 c = conn.cursor()
+
 app = Flask(__name__)
-pnr="SIE98"
-name = ""
+
 @app.route('/view')
 def dashboard():
     pnr = request.args.get('pnr')
     name = request.args.get('name')
-    print name  + " in view "
-
-
-    # name]re
-    # print(pnr)
-
-    # password = request.args.get('password')
-    # user_email = session["user"]
     c.execute("select transit_id from journeys where pnr = '{}'".format(pnr))
     transit_id = c.fetchone()[0]
-    print transit_id
     c.execute("select * from airplane_transits where transit_id = '{}'".format(transit_id))
-    transit_id ,date ,from_place ,to_place ,from_code,to_code,travel_time ,departure_time ,arrival_time ,flight_no=c.fetchone()
-    print date , flight_no
-    webbrowser.open('http://127.0.0.1:5000/dashboard?pnr='+pnr+'&name='+name, new=0)
-    # fname = name.split()[0]
+    transit_id, date, from_place, to_place, from_code, to_code, travel_time, departure_time, arrival_time, flight_no = c.fetchone()
+    webbrowser.open('http://127.0.0.1:5000/dashboard?pnr=' + pnr + '&name=' + name, new=0)
     return render_template("thankyou.html")
-
-# CREATE TABLE airplane_transits
-# transit_id ,date ,from_place ,to_place ,from_code,to_code,travel_time ,departure_time ,arrival_time ,flight_no
-
 
 
 @app.route("/dashboard")
-# @login_required
-def dashboard2():
-    print "hello"
-    # print pnr
-    # print name
+def dashboard():
     pnr = request.args.get('pnr')
     name = request.args.get('name')
-
     c.execute("select transit_id from journeys where pnr = '{}'".format(pnr))
     transit_id = c.fetchone()[0]
-    print transit_id
     c.execute("select * from airplane_transits where transit_id = '{}'".format(transit_id))
-    transit_id ,date ,from_place ,to_place ,from_code,to_code,travel_time ,departure_time ,arrival_time ,flight_no=c.fetchone()
-    print date , flight_no
-    # user_email = session["user"]
-    # c.execute("select name from user where email = '{}'".format(user_email))
-    # name = c.fetchone()[0]
-    # fname = name.split()[0]
-    # return render_template("dashboard.html", user="pree")
-    return render_template("b.html",flight_no=flight_no, from_place = from_place , to= to_place, name=name)
-
-
-
+    transit_id, date, from_place, to_place, from_code, to_code, travel_time, departure_time, arrival_time, flight_no = c.fetchone()
+    return render_template("b.html", flight_no=flight_no, from_place=from_place, to=to_place, name=name)
 
 
 app.run("0.0.0.0")
